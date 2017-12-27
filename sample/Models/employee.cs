ceb22e10c7a4716bbeac426c6077aa4bf30b99fc
sreevaniR <34880883+sreevaniR@users.noreply.github.com>
2017-12-27 15:29:32 +0530

    Add project files.

---
+++ sample/Models/employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sample.Models
+{
+    public class employee
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
